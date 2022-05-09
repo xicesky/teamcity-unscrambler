2ff2518b2c301019a34236f480041fbdbda51123
Markus Dangl <663399+xicesky@users.noreply.github.com>
2022-05-09 12:20:33 +0200

    Just add setup.py to make PyCharm happy

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='teamcity-unscrambler',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Jack Singleton',
+    author_email='git@jacksingleton.com',
+    description=''
+)
